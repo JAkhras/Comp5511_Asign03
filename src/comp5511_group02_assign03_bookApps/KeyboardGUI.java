4b729110be4f4a5dd22358c75430d340b8efd31d
JAkhras <johnakhras@me.com>
2016-11-11 13:22:11 -0500

    Included Aikens Binary tree and heap sort Aiken, please verify

--- src/comp5511_group02_assign03_bookApps/KeyboardGUI.java
+++ src/comp5511_group02_assign03_bookApps/KeyboardGUI.java
@@ -12,12 +12,13 @@ public class KeyboardGUI {
      BookDao bookDao = new BookArrayListData();
      
     public void GUI(){
+       
+        bookDao.sortCatalog();
          System.out.println(bookDao.listBooks());
         Scanner Type = new Scanner(System.in);
         String Function = "";        
         
         while(!Function.equals("e")) {
-        
         System.out.println("Please type first letter of your desired function ");
         System.out.println("'a' for adding, 'd' for deleting, 'u' for update, 's' for searching, 'e' for exit");
         Function = Type.nextLine();
@@ -38,8 +39,10 @@ public class KeyboardGUI {
         System.out.println("Please Type the Price");
         String PRICE = Type.nextLine();
         bookDao.addBook(ISBN, TITLE,AUTHOR,PUBLISHER,ADDRESS, PRICE);
+        bookDao.sortCatalog();
         System.out.println(bookDao.listBooks());
         System.out.println("You have Added:   " + ISBN);
+        System.out.println("Heap-Sorted book list is represented above");
         }
         else if(Function.equals("d")){
         System.out.println("Okay let's delete a record! ");
@@ -50,8 +53,10 @@ public class KeyboardGUI {
         //What if the value is not in the book?
         //could run a search first
         bookDao.deleteBook(ISBN);
+        bookDao.sortCatalog();
         System.out.println(bookDao.listBooks());
         System.out.println("You have deleted:   " + ISBN);
+        System.out.println("Heap-Sorted book list is represented above");
         }
        
         else if(Function.equals("u")){
@@ -70,8 +75,10 @@ public class KeyboardGUI {
         System.out.println("Please Type the updated Price");
         String PRICE = Type.nextLine();
         bookDao.updateBook(ISBN, TITLE,AUTHOR,PUBLISHER,ADDRESS, PRICE);
+        bookDao.sortCatalog();
         System.out.println(bookDao.listBooks());
         System.out.println("You have Updated:   " + ISBN);
+        System.out.println("Heap-Sorted book list is represented above");
         }
         
         else if(Function.equals("e")) {
@@ -88,32 +95,36 @@ public class KeyboardGUI {
         System.out.println("For ISBN type is, for Title type ti, for Author type au, for Publisher type pu, for Address type ad, for Price type pr");
         
         String Category = Type.nextLine();
+        bookDao.sortCatalog();
         System.out.println(bookDao.listBooks());
+        System.out.println("Heap-Sorted book list is represented above");
+        
         if(Category.equals("is")){
          Book book = bookDao.fetchIsbn(ISBN);
-        System.out.println(book.getIsbn());
+        System.out.println("isbn for your search is: "+book.getIsbn());
+         
             
         }
         else if(Category.equals("ti")){
          Book book = bookDao.fetchIsbn(ISBN);
-         System.out.println(book.getTitle());
+         System.out.println("Title for your search is: "+book.getTitle());
             
         }
         else  if(Category.equals("au")){
             Book book = bookDao.fetchIsbn(ISBN);
-            System.out.println(book.getAuthor());
+            System.out.println("Author name for your search is: "+book.getAuthor());
         }
         else if(Category.equals("pu")){
          Book book = bookDao.fetchIsbn(ISBN);
-        System.out.println(book.getPublisher());
+        System.out.println("Publisher Name for your search is: "+book.getPublisher());
         }
         else if(Category.equals("ad")){
             Book book = bookDao.fetchIsbn(ISBN);
-            System.out.println(book.getAddress());
+            System.out.println("Address for your search is: "+book.getAddress());
         }
         else if(Category.equals("pr")){
            Book book = bookDao.fetchIsbn(ISBN);
-            System.out.println(book.getPrice());
+            System.out.println("Price for your search is: "+book.getPrice());
         }
        
         }
